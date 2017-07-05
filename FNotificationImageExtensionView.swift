f81b0e5a4d1ed324aca390a1a8638feaf950948d
FirasAKAK <fir_khalidi@hotmail.com>
2017-07-05 14:26:45 +0300

    Update FNotificationImageExtensionView.swift

--- FNotificationImageExtensionView.swift
+++ FNotificationImageExtensionView.swift
@@ -2,8 +2,8 @@
 //  FNotificationImageExtensionView.swift
 //  FInAppNotification
 //
-//  Created by Apple on 7/5/17.
-//  Copyright © 2017 *TechnologySARL. All rights reserved.
+//  Created by Firas Al Khatib Al Khalidi on 7/5/17.
+//  Copyright © 2017 Firas Al Khatib Al Khalidi. All rights reserved.
 //
 
 import UIKit
